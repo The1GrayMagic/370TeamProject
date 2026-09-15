ed7a7590c4a004381f0c373ec48a23191ac91744
Grayson Bjork <140342558+The1GrayMagic@users.noreply.github.com>
2026-09-14 22:36:46 -0700

    update DayStatus

--- version1/src/DayStatus.java
+++ version1/src/DayStatus.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.Map;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -19,18 +20,23 @@ public class DayStatus {
         }
     }
 
-    public DayStatus(){
-
-    }
-
     //get date
     public LocalDate getDate() {
         return date;
     }
 
+    // get a list as set of all times
+    public Set<LocalTime> geTimes() {
+        return completions.keySet();
+    }
+
+    //gets map of key and value
+    public Map<LocalTime, Boolean> getCompletions() {
+        return completions;
+    }
+
     // input the update value, only if in the list
     public boolean updateInList(LocalTime lT, boolean updateValue) {
-
         if (completions.containsKey(lT)) {
             completions.put(lT, updateValue);
             return true;
--- version1/src/habit.java
+++ version1/src/habit.java
@@ -14,7 +14,6 @@ public class habit {
     //description of the habit
     String purpose;
 
-
     // map (day of the week) to a (time of expected time to complete the habit)
     // the list allow use to change time or have more than one
     Map<DayOfWeek, List<LocalTime>> timesOfWeek  = new HashMap<>();
@@ -26,12 +25,40 @@ public class habit {
 //---------------------------------------------------------------------------------------------------
 
 
-
-    public habit() {
-
-
-
+    // call with just name and description
+    public habit(String nameV,String purposeV) {
+        name = nameV;
+        purpose = purposeV;
     }
+    // call but with day listed, no times tho
+    public habit(String nameV,String purposeV,List<DayOfWeek> daylist) {
+        name = nameV;
+        purpose = purposeV;
+
+        for(DayOfWeek day : daylist) {
+            timesOfWeek.put(day,null);
+        }
+    }
+    //call with everything
+    public habit(String nameV,String purposeV,List<DayOfWeek> daylist,List<LocalTime> timeList) {
+        name = nameV;
+        purpose = purposeV;
+
+        for (DayOfWeek day : daylist) {
+            timesOfWeek.put(day,timeList);
+        }
+    }
+
+
+    //way to import old history
+    public void addPreiousHistory(List<DayStatus> oldHistory){
+        history = oldHistory;
+    }
+
+    public void addHistory(DayStatus newDay){
+        history.add(newDay);
+    }
+
 
     // adds to the week array the time
     // to call name.addTime(DayOfWeek.MONDAY, LocalTime.of(8, 30));
@@ -47,7 +74,16 @@ public class habit {
         timesOfWeek.get(day).add(time);
     }
 
+    // remove time that user wants to do
+    public void removeTime(DayOfWeek day, LocalTime time) {
+
+        if (timesOfWeek.containsKey(day)) {
+            timesOfWeek.get(day).remove(time);
+        }
+    }
+
     //get a list of days
+    // 0 is Sunday,1 is Monday, ... 6 is Saturday
     public boolean[] getDays(){
 
         boolean[] days = new boolean[7];
@@ -63,16 +99,15 @@ public class habit {
         return days;
     }
 
-
     //get the name
-    public String getName() {
-        return name;
-    }
+    public String getName()  {return name;}
     //get the purpose
-    public String getPurpose() {
-        return purpose;
-    }
+    public String getPurpose() {return purpose;}
 
+    //Update the description of habit
+    public void updatePurpose(String newP) {purpose=newP;}
 
+    // set new name, not sure if this is needed. Like make new habit?
+    public void newName(String newNJustMakeANewHabit) {name = newNJustMakeANewHabit;}
 
 }
